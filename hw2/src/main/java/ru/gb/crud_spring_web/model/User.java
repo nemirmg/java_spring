e972b973ebcd6c50453cf72d9d3bbe2df239c9aa
nemirmg <156666849+nemirmg@users.noreply.github.com>
2025-01-09 13:10:13 +0300

    Add files via upload

---
+++ hw2/src/main/java/ru/gb/crud_spring_web/model/User.java
@@ -0,0 +1,46 @@
+package ru.gb.crud_spring_web.model;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(firstName, user.getFirstName()) && Objects.equals(lastName, user.getLastName());
+    }
+}
